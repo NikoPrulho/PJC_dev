b7ab18e8484f649a22537aebc5ab734a1b0ac312
NikoPrulho <159452411+NikoPrulho@users.noreply.github.com>
2024-09-22 18:24:21 +1200

    Update scriptes.js

--- scriptes/scriptes.js
+++ scriptes/scriptes.js
@@ -1,3 +1,36 @@
+function adjustFontSize() {
+    var width = window.innerWidth;
+    var root = document.documentElement;
+
+    if (width >= 1024) {
+        root.style.setProperty('--font-base-size', '16px');
+    } else if (width >= 910) {
+        root.style.setProperty('--font-base-size', '15px');
+    } else if (width >= 850) {
+        root.style.setProperty('--font-base-size', '14px');
+    } else if (width >= 785) {
+        root.style.setProperty('--font-base-size', '13px');
+    } else if (width >= 650) {
+        root.style.setProperty('--font-base-size', '12px');
+    } else if (width >= 576) {
+        root.style.setProperty('--font-base-size', '11px');
+    } else if (width >= 465) {
+        root.style.setProperty('--font-base-size', '10px');
+    } else if (width >= 398) {
+        root.style.setProperty('--font-base-size', '9px');
+    } else if (width >= 290) {
+        root.style.setProperty('--font-base-size', '8px');
+    } else {
+        root.style.setProperty('--font-base-size', '6px');
+    }
+}
+
+adjustFontSize();
+
+window.addEventListener('resize', adjustFontSize);
+
+
+
 // fontion pour ouvrir le menu de  navigation pour tel et tablette
 const menuToggle = document.getElementById('menu_icon');
 const mobileMenu = document.getElementById('mobile_menu');
